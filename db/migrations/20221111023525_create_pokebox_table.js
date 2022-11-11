d4e677cd6127e7a60d799bb712ee6295a3c075a6
Yusei Harigaya <harryusei@harrymac.local>
2022-11-11 15:04:59 +0900

    fix lint - quote:double

--- db/migrations/20221111023525_create_pokebox_table.js
+++ db/migrations/20221111023525_create_pokebox_table.js
@@ -3,24 +3,25 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('poke_box', function (table) {
-    table.increments('id').primary();
-    table.string('species');
-    table.string('nickname');
-    table.string('ability');
-    table.string('nature');
-    table.string('item');
-    table.integer('eff_h');
-    table.integer('eff_a');
-    table.integer('eff_b');
-    table.integer('eff_c');
-    table.integer('eff_d');
-    table.integer('eff_s');
-    table.string('move_1');
-    table.string('move_2');
-    table.string('move_3');
-    table.string('move_4');
-    table.string('description');
+  return knex.schema.createTable("poke_box", function (table) {
+    table.increments("id").primary();
+    table.string("species");
+    table.string("species_en");
+    table.string("nickname");
+    table.string("ability");
+    table.string("nature");
+    table.string("item");
+    table.integer("eff_h");
+    table.integer("eff_a");
+    table.integer("eff_b");
+    table.integer("eff_c");
+    table.integer("eff_d");
+    table.integer("eff_s");
+    table.string("move_1");
+    table.string("move_2");
+    table.string("move_3");
+    table.string("move_4");
+    table.string("description");
   });
 };
 
@@ -29,5 +30,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  knex.schema.dropTable('pokeBox');
+  knex.schema.dropTable("pokeBox");
 };
--- test/_pokeBox.js
+++ test/_pokeBox.js
@@ -1,11 +1,11 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 chai.should();
-const { setupServer } = require('../src/server');
+const { setupServer } = require("../src/server");
 
 const server = setupServer();
-describe('pokebox API Server', () => {
+describe("pokebox API Server", () => {
   let req;
   beforeEach(() => {
     req = chai.request(server).keepOpen();
@@ -14,10 +14,10 @@ describe('pokebox API Server', () => {
     req.close();
   });
 
-  describe('GET /api/pokebox/ping - test', () => {
-    it('should return pong', async () => {
-      const res = await req.get('/api/pokebox/ping');
-      const pong = 'pong';
+  describe("GET /api/pokebox/ping - test", () => {
+    it("should return pong", async () => {
+      const res = await req.get("/api/pokebox/ping");
+      const pong = "pong";
       res.should.be.html;
       res.text.should.equal(pong);
     });
