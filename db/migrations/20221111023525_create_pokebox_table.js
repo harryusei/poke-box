95a541d77b093972064aa1ab78d1cb8ea1f6dee9
Yusei Harigaya <harryusei@harrymac.local>
2022-11-11 13:45:05 +0900

    initial migration & seed

---
+++ db/migrations/20221111023525_create_pokebox_table.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("poke_box", function (table) {
+    table.increments("id").primary();
+    table.string("species");
+    table.string("nickname");
+    table.string("ability");
+    table.string("nature");
+    table.string("item");
+    table.integer("eff_h");
+    table.integer("eff_a");
+    table.integer("eff_b");
+    table.integer("eff_c");
+    table.integer("eff_d");
+    table.integer("eff_s");
+    table.string("move_1");
+    table.string("move_2");
+    table.string("move_3");
+    table.string("move_4");
+    table.string("description");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  knex.schema.dropTable("pokeBox");
+};
