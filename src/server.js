ae1508c6b918c64fc2a0caebc2bd28284473e02c
Yusei Harigaya <harryusei@harrymac.local>
2022-11-11 14:39:34 +0900

    fix lint

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const setupServer = () => {
   const app = express();
   app.use(express.json());
 
-  app.get("/api/pokebox/ping", (req, res) => {
-    res.send("pong");
+  app.get('/api/pokebox/ping', (req, res) => {
+    res.send('pong');
   });
 
   return app;
---
+++ test/_pokeBox.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+chai.should();
+const { setupServer } = require('../src/server');
+
+const server = setupServer();
+describe('pokebox API Server', () => {
+  let req;
+  beforeEach(() => {
+    req = chai.request(server).keepOpen();
+  });
+  afterEach(() => {
+    req.close();
+  });
+
+  describe('GET /api/pokebox/ping - test', () => {
+    it('should return pong', async () => {
+      const res = await req.get('api/pokebox/ping');
+      const pong = 'pong';
+      res.should.be.html;
+      res.text.should.equal(pong);
+    });
+  });
+});
