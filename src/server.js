5511363edc2451bc9ba22a1e1819242badc8d8b7
Yusei Harigaya <harryusei@harrymac.local>
2022-11-11 11:07:41 +0900

    initialized server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+
+  app.get("/api/pokebox/ping", (req, res) => {
+    res.send("pong");
+  });
+
+  return app;
+};
+
+module.exports = { setupServer };
